f2264399fc5af5f7e5bd4f627cea5e961d98c0f0
kartik.iyer <kartik.iyer@quickstart.cloudera>
2017-06-25 00:18:27 +0530

    File Merger v1.1
    Implemented ToolInterface and made changes to use CombineTextInputFormat

--- HadoopVmWorks/src/com/kartikiyer/hadoop/filemerge/FileMerge.java
+++ HadoopVmWorks/src/com/kartikiyer/hadoop/filemerge/FileMerge.java
@@ -1,34 +1,40 @@
 package com.kartikiyer.hadoop.filemerge;
 
 
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 
-
-
-
-public class FileMerge
+public class FileMerge extends Configured implements Tool
 {
 	public static void main(String[] args) throws Exception
 	{
 		System.setProperty("hadoop.home.dir", "C:\\winutil\\");
 		System.setProperty("HADOOP_USER_NAME", "cloudera");
 
+		ToolRunner.run(new FileMerge(), args);
+	}
+
+	@Override
+	public int run(String[] args) throws Exception
+	{
 		Job job = Job.getInstance();
 		job.setJobName("FileMerger");
 
 		job.getConfiguration().set("mapreduce.app-submission.cross-platform", "true");
-//		job.getConfiguration().set("mapred.job.reuse.jvm.num.tasks", "-1");
-		
+		// job.getConfiguration().set("mapred.job.reuse.jvm.num.tasks", "-1");
+
 
 		job.setJarByClass(FileMerge.class);
 		job.setMapperClass(FileMergeMapper.class);
@@ -37,7 +43,7 @@ public class FileMerge
 		job.setMapOutputKeyClass(LongWritable.class);
 		job.setMapOutputValueClass(Text.class);
 
-		job.setInputFormatClass(TextInputFormat.class);
+		job.setInputFormatClass(CombineTextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 
 		job.setOutputKeyClass(Text.class);
@@ -46,12 +52,11 @@ public class FileMerge
 		job.setNumReduceTasks(1);
 
 
-		Path in = new Path("/user/cloudera/weatherInput/files1");
-//		Path in = new Path("/user/kartik.iyer/weatherInput/files");
+		Path in = new Path("/user/cloudera/Small_Files");
 		FileInputFormat.addInputPath(job, in);
 
 		FileSystem fs = FileSystem.get(job.getConfiguration());
-		Path out = new Path("/user/cloudera/weatherOutput");
+		Path out = new Path("/user/cloudera/mergedop");
 
 		if (fs.exists(out))
 			fs.delete(out, true);
@@ -59,7 +64,7 @@ public class FileMerge
 		FileOutputFormat.setOutputPath(job, out);
 
 		System.out.println("Starting job");
-		job.waitForCompletion(true);
 
+		return job.waitForCompletion(true) ? 1 : 0;
 	}
 }
